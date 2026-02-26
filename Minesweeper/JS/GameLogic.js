ff9b9ee5541f2009f7459779fa741500d81c64a2
RoyHerrera <roy.herrera@alumnos.uai.edu.ar>
2026-02-26 07:02:18 -0300

    changed default mines

--- Minesweeper/JS/GameLogic.js
+++ Minesweeper/JS/GameLogic.js
@@ -4,9 +4,8 @@
 function updateMineCounter() {
     var counter = document.getElementById("mine-counter");
     var remaining = bombsTotal - flagsPlaced;
-    
     counter.textContent = remaining.toString().padStart(3, "0");
 }
 
-boardSetup(8, 8, 8)
+boardSetup(8, 8, 10)
 updateMineCounter();
